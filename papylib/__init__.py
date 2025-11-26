6eadd83d73c657c1226a1846d20fc3ca1f3156a7
MuskanShinde99 <muskan.shinde.99@gmail.com>
2025-11-26 15:53:31 +0100

    Refactor notebook utilities into shared module

---
+++ papylib/__init__.py
@@ -0,0 +1,9 @@
+"""Utility package for shared notebook helpers."""
+
+from .notebook_utils import compute_modes_rms, compute_response_matrix
+
+__all__ = [
+    "compute_modes_rms",
+    "compute_response_matrix",
+]
+
